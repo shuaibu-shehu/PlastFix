e33513b788f5c51077e49ff449b2bc78912e70b3
shuaibu-shehu <shuaibushehukalifa@gmail.com>
2025-01-04 12:42:49 +0530

    recent kist page

---
+++ src/components/dashboard/recent-list.tsx
@@ -0,0 +1,11 @@
+import { getPlasticItems } from '@/lib/actions/actions'
+import React from 'react'
+
+async function RecentList() {
+    // const list = await getPlasticItems()
+  return (
+    <div>RecentList</div>
+  )
+}
+
+export default RecentList
